3e94414e5e691acf6d231911c54e0a344247d3af
Roshin C <idroshin2010@gmail.com>
2025-04-07 21:26:40 -0400

    Initial Commit

---
+++ src/main/java/dev/roshin/login/demogitlaboauth/DemoGitlabOauthApplication.java
@@ -0,0 +1,13 @@
+package dev.roshin.login.demogitlaboauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitlabOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitlabOauthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/roshin/login/demogitlaboauth/DemoGitlabOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.roshin.login.demogitlaboauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitlabOauthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
